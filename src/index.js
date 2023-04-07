20a856560f365a0853df6bdb0551491a1c6377d0
Ezequielprograma <ezequiel.medina.profesional@gmail.com>
2023-04-07 02:53:05 -0300

    Lección 2: Elementos Anidados

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'; /*Conexion a la hoja de estilos */
 import App from './App'; /*Importamos la clase APP */
 import reportWebVitals from './reportWebVitals';
 
-/*Creación y renderización de elementos */
+/*LECCION 1: Creación y renderización de elementos */
 
 /*Forma 1 */
   ReactDOM.render(<App/>,document.getElementById('root'));
@@ -19,5 +19,26 @@ import reportWebVitals from './reportWebVitals';
 /*Forma 4 */
   const element1 = React.createElement('p',{align: 'center'},'Nuevo Parrafo');
   ReactDOM.render(element1,document.getElementById('parrafo'));
+
+
+
+/*LECCION 2: Elementos Anidados */
+
+//creamos elementos por separado
+var listElement1 = React.createElement('li',{id:'item1',type:'circle'},'Uno');
+var listElement2 = React.createElement('li',{id:'item2',type:'disc'},'Dos');
+var listElement3 = React.createElement('li',{id:'item3',type:'square'},'Tres');
+var listElement4 = React.createElement('li',{id:'item4',type:'1'},'Cuatro');
+var listElement5 = React.createElement('li',{id:'item5',type:'a'},'Cinco');
+var listElement6 = React.createElement('li',{id:'item6',type:'A'},'Seis');
+
+//creamos un arreglo de elementos, en este caso los items de una lista
+var elements = [listElement1,listElement2,listElement3,listElement4,listElement5,listElement6];
+
+//definimos un elelento de tipo lista ordenada y le adjutamos el arreglo anterior
+var list0Elements = React.createElement('ul','',elements);
+
+//renderizamos el elemento en el contenedor con el id "list"
+ReactDOM.render(list0Elements,document.getElementById('list'));
 
 reportWebVitals();
