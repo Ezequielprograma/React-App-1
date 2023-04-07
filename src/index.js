a24303660f567c8a924e8b9f766e54bdd0f51923
Ezequielprograma <ezequiel.medina.profesional@gmail.com>
2023-04-07 00:20:41 -0300

    Lección 1:Creación y renderización de Elementos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'; /*importamos la librería */
+import ReactDOM from 'react-dom'; /*importamos la librería */
+import './index.css'; /*Conexion a la hoja de estilos */
+import App from './App'; /*Importamos la clase APP */
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/*Creación y renderización de elementos */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+/*Forma 1 */
+  ReactDOM.render(<App/>,document.getElementById('root'));
+
+/*Forma 2 */
+  ReactDOM.render(<div>Nuevo Nodo Html</div>,document.getElementById("html"));
+
+/*Forma 3 */
+  const element = (<div> Son las {new Date().toLocaleTimeString()}</div>);
+  ReactDOM.render(element,document.getElementById("time"));
+
+/*Forma 4 */
+  const element1 = React.createElement('p',{align: 'center'},'Nuevo Parrafo');
+  ReactDOM.render(element1,document.getElementById('parrafo'));
+
 reportWebVitals();
