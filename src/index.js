58c98af1a228e7cd5d32748f4fb1aa3de1e3e55b
Ezequielprograma <ezequiel.medina.profesional@gmail.com>
2023-04-11 20:24:29 -0300

    Ejercicio 4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'; /*importamos la librería */
 import ReactDOM from 'react-dom'; /*importamos la librería */
 import './index.css'; /*Conexion a la hoja de estilos */
-
 import reportWebVitals from './reportWebVitals';
+
 
 /*Ejercicio 1 */
 const element = (<div> To get stared, edit {<code>src/App.js</code>} and save {<b>reload.</b>}</div>);
@@ -52,6 +52,51 @@ const E3_formulario = (
             </fieldset>
         </form>
 );
+
 ReactDOM.render(E3_formulario,document.getElementById('E3-formulario'));
+
+/*Ejercicio 4*/
+ReactDOM.render(React.createElement('img',{src: require('./photo.jpg'),alt:'Pan',class: 'img_pan'}),document.getElementById('E4-img'));
+ReactDOM.render(React.createElement('h1',{align: 'center'},'Receta de Pan, fácil de hacer'),document.getElementById('E4-tituloCuerpo'));
+
+ReactDOM.render(<p>A continuación se indican los ingredientes necesarios para hacer la receta de pan:</p>,document.getElementById('E4-SubtituloCuerpo1'));
+
+var listElement1 = React.createElement('li',{id:'item1',type:'circle'},<b>15 gr</b>,' levadura frezca');
+var listElement2 = React.createElement('li',{id:'item2',type:'circle'},<b>1cda</b>,' azúcar');
+var listElement3 = React.createElement('li',{id:'item3',type:'circle'},<b>1cda</b>,' sal');
+var listElement4 = React.createElement('li',{id:'item4',type:'circle'},<b>3cda</b>,' de aceite de oliva');
+var listElement5 = React.createElement('li',{id:'item5',type:'circle'},<b>300 ml</b>,' agua templada (1 taza 1/2)');
+var listElement6 = React.createElement('li',{id:'item6',type:'circle'},<b>500 gr</b>,' harina');
+
+//creamos un arreglo de elementos, en este caso los items de una lista
+var elements = [listElement1,listElement2,listElement3,listElement4,listElement5,listElement6];
+
+//definimos un elelento de tipo lista ordenada y le adjutamos el arreglo anterior
+var recetaDePan = React.createElement('ul','',elements);
+
+//renderizamos el elemento en el contenedor con el id "list"
+ReactDOM.render(recetaDePan,document.getElementById('E4-list'));
+
+ReactDOM.render(<p>Ahora que ya tienes los ingredientes medidos y preparados sigue los siguientes pasos para lograr hacer tu pan:</p>,document.getElementById('E4-SubtituloCuerpo-2'));
+
+var listElement7 = React.createElement('li',{id:'item7',type:'1'},'Primero hay que hidratar la levadura. Pon la levadura con un poco de agua, el aceite y el azúcar. Revuelve hasta que este todo disuelto.');
+var listElement8 = React.createElement('li',{id:'item8',type:'1'},'Incorporar 5 cdas. De harina y cuando esté todo integrado dejar reposar aprox 10 minutos. Tapado con repasador.');
+var listElement9 = React.createElement('li',{id:'item9',type:'1'},'Terminar de incorporar la harina junto con la sal y el agua.');
+var listElement10 = React.createElement('li',{id:'item10',type:'1'},'Amasar durante unos 2 minutos aprox. Y dejar reposar unos 30 minutos.');
+var listElement11 = React.createElement('li',{id:'item11',type:'1'},'Saca el aire amasando un poco más y forma un bollo que debes colocar en la asadera aceitada.');
+var listElement12 = React.createElement('li',{id:'item12',type:'1'},'Aplica unos cortes y espolvorea con harina');
+var listElement13 = React.createElement('li',{id:'item13',type:'1'},'Tapa con un recipiente apto de horno y aceitado. Esto es para conservar la humedad.');
+var listElement14 = React.createElement('li',{id:'item14',type:'1'},'Por último lleva al horno recien encendido a 200 grados celsius, fuego arriba y abajo, durante 45 minutos aprox.');
+//creamos un arreglo de elementos, en este caso los items de una lista
+var elements2 = [listElement7,listElement8,listElement9,listElement10,listElement11,listElement12,listElement13,listElement14];
+
+//definimos un elelento de tipo lista ordenada y le adjutamos el arreglo anterior
+var recetaDePan2 = React.createElement('ol','',elements2);
+
+//renderizamos el elemento en el contenedor con el id "list"
+ReactDOM.render(recetaDePan2,document.getElementById('E4-list-2'));
+
+ReactDOM.render(<p>Ya haz hecho tu primer pan, ahora disfrútalo.</p>,document.getElementById('E4-SubtituloCuerpo-3'));
+
 reportWebVitals();
 
